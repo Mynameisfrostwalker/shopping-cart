3e492b65350d24452d6033725831878696ae4436
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-28 22:06:53 +0100

    created routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import "./css/App.css";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/shop" component={Shop} />
+    </Switch>
+  </HashRouter>
+);
 
 export default App;
