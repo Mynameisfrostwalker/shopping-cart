25ac2e52e7e87002a4f48f898dc9c9b710fcda29
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-28 22:07:12 +0100

    added font-awesome icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faShoppingCart,
+  faHome,
+  faShoppingBasket,
+} from "@fortawesome/free-solid-svg-icons";
 import "./css/reset.css";
 import "./css/index.css";
 import App from "./App";
+
+library.add(fab, faShoppingCart, faHome, faShoppingBasket);
 
 ReactDOM.render(
   <React.StrictMode>
