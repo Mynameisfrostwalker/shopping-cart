95bbd19368fc7a166fb87a8ad51a66fe542d2d41
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-28 22:09:33 +0100

    created main component

---
+++ src/components/MainHome.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../css/mainHome.module.css";
+
+const MainHome = () => (
+  <main className={styles.main}>
+    <div className={styles.center}>
+      <h2>Online store</h2>
+      <Link to="/shop">
+        <button type="button" className={styles.button}>
+          Shop
+        </button>
+      </Link>
+    </div>
+  </main>
+);
+
+export default MainHome;
