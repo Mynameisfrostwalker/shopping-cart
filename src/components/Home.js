3257e2419189cd88cfb4d1e1661c8d561793636c
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-28 22:07:54 +0100

    created Home page component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import MainHome from "./MainHome";
+import Footer from "./Footer";
+
+const Home = () => (
+  <div>
+    <Nav />
+    <MainHome />
+    <Footer />
+  </div>
+);
+
+export default Home;
