7042ee7a4cf4c936af5d8d49f12a6ffc99502f72
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-10-01 00:49:49 +0100

    Modified Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
 import navStyles from "../css/nav.module.css";
@@ -9,23 +10,47 @@ import navStyles from "../css/nav.module.css";
 
 /**
  * Navigation component
+ * @param {Object} param0 -Props
+ * @param {Number} param0.length - Cart length
  * @return {JSX}
  */
-const Nav = () => (
+const Nav = ({ length }) => (
   <nav className={navStyles.nav}>
     <div>
       <h1 className={navStyles.firstHeader}>Generic Online Store</h1>
     </div>
     <div className={navStyles.icon}>
-      <NavLink exact to="/" activeClassName={navStyles.active}>
+      <NavLink
+        exact
+        to="/"
+        activeClassName={navStyles.active}
+        className={navStyles.inactive}
+      >
         <FontAwesomeIcon icon="home" />
       </NavLink>
-      <NavLink to="/shop" activeClassName={navStyles.active}>
+      <NavLink
+        to="/shop"
+        activeClassName={navStyles.active}
+        className={navStyles.inactive}
+      >
         <FontAwesomeIcon icon="shopping-basket" />
       </NavLink>
-      <FontAwesomeIcon icon="shopping-cart" />
+      <NavLink
+        to="/cart"
+        activeClassName={navStyles.active}
+        className={navStyles.inactive}
+      >
+        <div className={navStyles.cartDiv}>
+          <FontAwesomeIcon icon="shopping-cart" />
+          <div className={navStyles.counter}>{length}</div>
+        </div>
+      </NavLink>
     </div>
   </nav>
 );
+
+Nav.propTypes = {
+  length: PropTypes.number.isRequired,
+};
 
 export default Nav;
